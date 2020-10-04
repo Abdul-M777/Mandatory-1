a1316c8fc28c1e4c6068d9e2cc97e2373c235fcb
Abdulmuaz Alshaikhli <abdu0445@edu.easj.dk>
2020-10-05 00:17:57 +0200

    Initial commit

---
+++ movie.js
@@ -0,0 +1,106 @@
+$(document).ready(function() {
+    $("#submit2").click(function () {
+        var validate = Validate2();
+        $("#message").html(validate);
+        if (validate.length == 0){
+            CallAPI2();
+        }
+    });
+
+
+function CallAPI2() {
+    $.ajax({
+        url: "https://api.themoviedb.org/3/search/movie?&query=" + $("#movieInput").val(),
+        data: { "api_key": "dc662b7f1d2f741b834a79e3ebf185e5" },
+        dataType: "json",
+        success: function (result2) {
+            var resultHtml2 = $("<div class=\"resultDiv\"><p>Movies</p>");
+             for (i = 0; i < result2["results"].length; i++) {
+
+                        var image = result2["results"][i]["poster_path"] == null ? "Image/no-image.png" : "https://image.tmdb.org/t/p/w500/" + result2["results"][i]["poster_path"];
+                        var date = result2["results"][i]["release_date"] == null ? "Unknown" : result2["results"][i]["release_date"].substr(0,4);
+
+                        resultHtml2.append("<div class=\"result2\" resourceId=\"" + result2["results"][i]["id"] + "\">"
+                        + "<img src=\"" + image + "\" />"
+                        + "<p><a>" + result2["results"][i]["title"] + " "+ result2["results"][i]["original_language"] + "</a></p>"
+                        + "<p><a>" + date +
+                        "</a></p></div>")
+                        console.log(result2);
+                    }
+                    
+            
+            resultHtml2.append("</div>");
+            $("#message").html(resultHtml2);
+        },
+        error: function (xhr, status, error) {
+            $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+        }
+    });
+}
+
+$("#message").on("click", ".result2", function () {
+    var resourceId2 = $(this).attr("resourceId");
+    $.ajax({
+        url: "https://api.themoviedb.org/3/movie/" + resourceId2 +"?"+ "api_key=dc662b7f1d2f741b834a79e3ebf185e5&append_to_response=credits",
+        
+        dataType: 'json',
+        success: function (result2) {
+            $("#modalTitleH4").html(result2["title"]);
+
+            var date = result2["release_date"] == null ? "Unknown" : result2["release_date"].substr(0,4);
+            var language = result2["spoken_languages"] == null ? "No information available" : result2["spoken_languages"][0]["name"];
+            var runtime = result2["runtime"] == null ? "" : "RunTime: "+result2["runtime"];
+            var overview = result2["overview"] == null ? "Not available" : result2["overview"];
+            
+            
+
+            var resultHtml2 = "<p>Release Date: "+date+"</p><p>" +"Language: "+ language + "</p>";
+            resultHtml2 += "<p>"+runtime + "</p>" +"<p>"+ overview + "</p>"+"<p>"+"HomePage: "+result2["homepage"]+"</p><p>Genre: </p>";
+            
+        for (i = 0; i < result2["genres"].length; i++) {
+           resultHtml2 += "<ul><li>"+result2["genres"][i]["name"]+"</li> </ul>";
+                
+        }
+
+        resultHtml2 += "<p>Production Companies: </p>";
+
+        for (i =0; i < result2["production_companies"].length; i++) {
+           resultHtml2 += "<ul><li>"+result2["production_companies"][i]["name"]+"</li></ul>";
+        }
+
+       
+        resultHtml2 += "<p>Cast: </p>";
+
+        for (i = 0; i < result2["credits"]["cast"].length; i++) {
+            resultHtml2 += "<ul><li>"+result2["credits"]["cast"][i]["name"]+" as "+result2["credits"]["cast"][i]["character"]+"</li></ul>";
+        }
+
+        resultHtml2 += "<p>Crew: </p>";
+
+        for (i = 0; i < result2["credits"]["crew"].length; i++) {
+            resultHtml2 += "<ul><li>"+result2["credits"]["crew"][i]["name"]+" -- "+result2["credits"]["crew"][i]["job"]+"</li></ul>";
+        }
+        
+        
+
+            
+            
+
+            $("#modalBodyDiv").html(resultHtml2)
+
+            $("#myModal").modal("show");
+        },
+        
+    });
+});
+
+
+
+function Validate2() {
+    var errorMessage = "";
+    if ($("#movieInput").val() == "") {
+        errorMessage += "Enter Movie Name";
+    }
+    return errorMessage;
+}
+});
---
+++ script.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    $("#submit").click(function () {
+        var validate = Validate();
+        $("#message").html(validate);
+        if (validate.length == 0){
+            CallAPI();
+        }
+    });
+
+    $("#message").on("click", ".result", function () {
+        var resourceId = $(this).attr("resourceId");
+        $.ajax({
+            url: "https://api.themoviedb.org/3/person/" + resourceId+"?"+ "api_key=dc662b7f1d2f741b834a79e3ebf185e5&append_to_response=credits",
+            
+            dataType: 'json',
+            success: function (result) {
+                $("#modalTitleH4").html(result["name"]);
+
+                var activity = result["known_for_department"] == null ? "Unknown" : result["known_for_department"]
+                var biography = result["biography"] == null ? "No information available" : result["biography"];
+                var deceased = result["deathday"] == null ? "" : "  /  "+result["deathday"];
+                var homepage = result["homepage"] == null ? "" : result["homepage"];
+
+                var resultHtml = "<p>Job: "+activity+"</p><p>" + biography + "</p>";
+                resultHtml += "<p>Birthday: " + result["birthday"]  + deceased + "</p><p>Place of Birth: " + result["place_of_birth"]+"</p><p>HomePage: <a href="+result["homepage"]+">" + homepage+ "</a></p>";
+
+                for (i = 0; i < result["credits"]["cast"].length; i++) {
+                    resultHtml += "<ul><li>"+result["credits"]["cast"][i]["title"]+" --- "+result["credits"]["cast"][i]["release_date"].substr(0,4)+" --- Acting"+"</li></ul>"
+                }
+
+                for (i = 0; i < result["credits"]["crew"].length; i++) {
+                    resultHtml += "<ul><li>"+result["credits"]["crew"][i]["title"]+" ----  "+result["credits"]["crew"][i]["release_date"].substr(0,4)+" ----  " +result["credits"]["crew"][i]["job"]+"</li></ul>"
+                }
+
+                $("#modalBodyDiv").html(resultHtml)
+
+                $("#myModal").modal("show");
+            },
+            error: function (xhr, status, error) {
+                $("#message").html("Result: " + status + " " + error + " " + xhr.status + " " + xhr.statusText) 
+            }
+        });
+    });
+
+   
+
+    function CallAPI() {
+        $.ajax({
+            url: "https://api.themoviedb.org/3/search/person?&query=" + $("#personInput").val(),
+            data: { "api_key": "dc662b7f1d2f741b834a79e3ebf185e5" },
+            dataType: "json",
+            success: function (result) {
+                var resultHtml = $("<div class=\"resultDiv\"><p>Names</p>");
+                 for (i = 0; i < result["results"].length; i++) {
+
+                            var image = result["results"][i]["profile_path"] == null ? "Image/no-image.png" : "https://image.tmdb.org/t/p/w500/" + result["results"][i]["profile_path"];
+
+                            resultHtml.append("<div class=\"result\" resourceId=\"" + result["results"][i]["id"] + "\">" + "<img src=\"" + image + "\" />" + "<p><a>" + result["results"][i]["name"] + "</a></p>" + "<p><a>" + result["results"][i]["known_for_department"] + "</a></p></div>")
+
+                        }
+                
+                resultHtml.append("</div>");
+                $("#message").html(resultHtml);
+            },
+           
+        });
+    }
+
+    function Validate() {
+        var errorMessage = "";
+        if ($("#personInput").val() == "") {
+            errorMessage += "Enter Person Name";
+        }
+        return errorMessage;
+    }
+   
+    
+    
+});
